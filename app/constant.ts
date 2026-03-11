b8f665bf1e11d7b0e1dc4f4e3c83e58bbdcd6f56
XiangRao <131976564+ricardrao@users.noreply.github.com>
2026-03-11 02:00:40 -0700

    add gpt-5.4-pro

--- app/constant.ts
+++ app/constant.ts
@@ -523,6 +523,8 @@ const openaiModels = [
   "gpt-5-nano",
   "gpt-5",
   "gpt-5-chat-2025-01-01-preview",
+  "gpt-5.4",
+  "gpt-5.4-pro",
   "gpt-4o",
   "gpt-4o-2024-05-13",
   "gpt-4o-2024-08-06",
